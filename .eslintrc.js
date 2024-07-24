3ce2153bb14c762adf3d5ef68e4e87dc607c376e
micronn <nanami_tanida@rebuild-inc.co.jp>
2024-07-24 17:09:10 +0900

    [update]  prettier & ESLint settings yarn add -D prettier prettier-plugin-organize-imports @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-plugin-tailwindcss eslint-config-prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: 'all',
+    printWidth: 80,
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    plugins: ['prettier-plugin-organize-imports'],
+}
